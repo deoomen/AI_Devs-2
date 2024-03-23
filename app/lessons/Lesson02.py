ce3e62fa762046c1e32303e528f4b453aca75216
deoomen <deoomen@protonmail.com>
2024-03-23 10:20:35 +0100

    feat: :sparkles: lesson 02 - moderation

---
+++ app/lessons/Lesson02.py
@@ -0,0 +1,32 @@
+import os
+from openai import OpenAI
+
+class Lesson02:
+    """
+    Lesson 02 - moderation
+    """
+
+    AI_Devs = None
+
+    def __init__(self, AI_Devs) -> None:
+        self.AI_Devs = AI_Devs
+
+    def run(self) -> None:
+        print('Lesson 02 - moderation')
+        self.AI_Devs.authorize('moderation')
+
+        task = self.AI_Devs.getTask()
+        print('The task is: ', task)
+
+        answer = []
+        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+        for sentence in task['input']:
+            result = client.moderations.create(
+                input=sentence,
+                model='text-moderation-latest'
+            )
+            answer.append(1 if result.results[0].flagged else 0)
+
+        print(answer)
+        self.AI_Devs.answer(answer)
--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import os
 import sys
 import AI_Devs
 from lessons import Lesson01
+from lessons import Lesson02
 
 def initLoggers() -> None:
-    level = logging.DEBUG
+    level = logging.INFO
     root = logging.getLogger()
     root.setLevel(level)
 
@@ -22,14 +23,21 @@ if __name__ == '__main__':
     try:
         apiKey = os.getenv('API_KEY')
         ai = AI_Devs.AI_Devs(apiKey)
-        lessonNumber = input("""
+
+        if 1 == len(sys.argv):
+            lessonNumber = input("""
 Which lesson do you want to run?
 1 - helloapi
+2 - moderation
 
 """)
+        else:
+            lessonNumber = sys.argv[1]
 
         if '1' == lessonNumber:
             lesson = Lesson01.Lesson01(ai)
+        elif '2' == lessonNumber:
+            lesson = Lesson02.Lesson02(ai)
         else:
             raise RuntimeError('Unknown lesson "{}"'.format(lessonNumber))
 
